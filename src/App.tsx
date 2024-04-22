2ec3f36316ab4411b2fbcac44d2c0884b15ce4d7
bryan-ahnlab <bryan@ahnlab.io>
2024-04-22 15:05:32 +0900

    feat: apply wallet connect library

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import {
+  getClient,
+  handleConnect,
+  handleDisconnect,
+  handleRequestTransaction,
+} from "./lib/wallet-connect";
+import { SignClient } from "@walletconnect/sign-client";
 
 function App() {
+  const [curSignClient, setCurSignClient] = useState<InstanceType<
+    typeof SignClient
+  > | null>(null);
+
+  useEffect(() => {
+    if (!curSignClient) {
+      const initializeClient = async () => {
+        const signClient = await getClient();
+        if (signClient) {
+          setCurSignClient(signClient);
+        }
+      };
+      initializeClient();
+    }
+    console.log("curSignClient: ", curSignClient);
+  }, [curSignClient]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Sign v2 Standalone</h1>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem",
+          alignItems: "center",
+        }}
+      >
+        <button
+          style={{ width: "80%", height: "3rem" }}
+          onClick={() => handleConnect()}
+          disabled={!curSignClient}
         >
-          Learn React
-        </a>
-      </header>
+          Connect
+        </button>
+        <button
+          style={{ width: "80%", height: "3rem" }}
+          onClick={() => handleDisconnect()}
+          disabled={!curSignClient}
+        >
+          Disconnect
+        </button>
+        <button
+          style={{ width: "80%", height: "3rem" }}
+          onClick={() => handleRequestTransaction()}
+          disabled={!curSignClient}
+        >
+          Request Transaction
+        </button>
+      </div>
     </div>
   );
 }
